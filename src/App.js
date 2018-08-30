38aa1b0a4d8fc385e744219cca5a598f2b60d136
AntonEmery <antonemery@gmail.com>
2018-08-30 09:45:30 -0700

    returning data from api

--- src/App.js
+++ src/App.js
@@ -4,6 +4,18 @@ import './App.css';
 import LocationForm from './components/LocationForm';
 
 class App extends Component {
+
+  submitLocation = (location) => {
+    this.setState({ loading: true }, () => {
+      fetch(`http://api.apixu.com/v1/current.json?key=06afaabe82054526aca231633182908&q=${location}`)
+      .then(data => {
+        return data.json();
+      }).then(result => {
+          console.log(result)
+      })
+    })
+  }
+
   render() {
     return (
       <div className="App">
@@ -14,7 +26,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <LocationForm />
+        <LocationForm submitLocation={this.submitLocation}/>
       </div>
     );
   }
