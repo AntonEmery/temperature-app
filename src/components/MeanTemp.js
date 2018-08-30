d6766f59c1ccf0afe6993ff9bdb23d2c7116ba9d
AntonEmery <antonemery@gmail.com>
2018-08-30 12:40:41 -0700

    displaying bar chart

--- src/components/MeanTemp.js
+++ src/components/MeanTemp.js
@@ -1,15 +1,28 @@
 import React, { Component }from 'react';
 import PropTypes from 'prop-types';
+import { BarChart } from 'react-easy-chart';
+
+
 
 class MeanTemp extends Component {
   state = {  };
 
   render() {
+
+    const { maxtemp_c, mintemp_c } = this.props;
     return (
       <div>
         <p>Mean Temp</p>
-        <p>{this.props.maxtemp_c}</p>
-        <p>{this.props.mintemp_c}</p>
+        <BarChart
+          axes
+          colorBars
+          width={250}
+          data={[
+            {x: 'Max', y: maxtemp_c},
+            {x: 'Min', y: mintemp_c}
+          ]}
+
+        />
       </div>
     );
   }
