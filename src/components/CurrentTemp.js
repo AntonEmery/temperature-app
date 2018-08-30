562cf3a8409f4ca80ebef34399a84ad5decc3a7b
AntonEmery <antonemery@gmail.com>
2018-08-30 11:33:01 -0700

    toggling between C and F

--- src/components/CurrentTemp.js
+++ src/components/CurrentTemp.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function CurrentTemp({temp_c, temp_f}) {
-  return (
-    <div>
-      <h3>Current Temperature</h3>
-      <p>Fahrenheit: {temp_f}</p>
-      <p>Celsius: {temp_c}</p>
-    </div>
-  )
+
+class CurrentTemp extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      celsius: true
+    }
+  }
+
+  render() {
+    const { temp_f, temp_c } = this.props;
+    return (
+      <div>
+        <h3>Current Temperature</h3>
+        {this.state.celsius
+        ? <p>Celsius: {temp_c}</p>
+        : <p>Fahrenheit: {temp_f}</p>}
+        <button onClick={() => this.setState(prevState => ({celsius: !prevState.celsius})) }>Choose Celsius or Farenheit</button>
+      </div>
+    )
+  }
 }
 
 export default CurrentTemp;
