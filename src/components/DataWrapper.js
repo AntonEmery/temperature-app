e4df8545a42dd84356c467b2cd73f6566b639c72
AntonEmery <antonemery@gmail.com>
2018-08-31 14:53:11 -0700

    toggling between daily and weekly

--- src/components/DataWrapper.js
+++ src/components/DataWrapper.js
@@ -1,21 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import CurrentTemp from './CurrentTemp';
 import MeanTemp from  './MeanTemp';
 import AggregrateTemps from './AggregateTemps';
 import WeeklyTemps from './WeeklyTemps';
 
-function DataWrapper({ maxTemp, minTemp, currentTemp, units, toggleTemp, tempHistory, weeklyTemps }) {
+class DataWrapper extends Component {
+  constructor(props) {
+    super(props)
 
+    this.state = {
+      view: 'daily'
+    }
+  }
+  render() {
+    const { maxTemp, minTemp, currentTemp, units, toggleTemp, tempHistory, weeklyTemps } = this.props;
     return (
       <div>
         <h3>Current Temperature Data in {units}</h3>
         <button className="btn" onClick={toggleTemp}>Toggle Celsius or Farenheit</button>
         <CurrentTemp currentTemp={currentTemp} />
         <MeanTemp maxTemp={maxTemp} minTemp={minTemp} />
-        <AggregrateTemps tempHistory={tempHistory} units={units}/>
-        <WeeklyTemps weeklyTemps={weeklyTemps} />
+        <button className="btn change-view" onClick={() => this.setState({ view: 'daily' })}>View Daily</button>
+        <button className="btn change-view" onClick={() => this.setState({ view: 'weekly' })}>View Weekly</button>
+        <button className="btn change-view" onClick={toggleTemp}>View Monthly</button>
+
+        {this.state.view === 'daily' && <AggregrateTemps tempHistory={tempHistory} units={units}/>}
+        {this.state.view === 'weekly' && <WeeklyTemps weeklyTemps={weeklyTemps} />}
       </div>
     )
+  }
 }
 
 export default DataWrapper;
--- src/components/WeeklyTemps.js
+++ src/components/WeeklyTemps.js
@@ -1,27 +1,16 @@
 import React, { Component } from 'react';
-import { BarChart } from 'react-easy-chart';
 
 
 class WeeklyTemps extends Component {
-  state = {  };
 
   render() {
-
     const { weeklyTemps } = this.props;
-    console.log(weeklyTemps.reduce((a, b) => a + b.temp, 0)/weeklyTemps.length)
-
-
+    const avgWeeklyTemp = weeklyTemps.reduce((a, b) => a + b.temp, 0) / weeklyTemps.length;
     return (
-      // wait until array populated to render graph
-     <BarChart
-          axes
-          colorBars
-          width={450}
-          height={400}
-          data={[
-
-          ]}
-        />
+      <div>
+        <p>Average Weekly Temperature</p>
+        <p>{ avgWeeklyTemp.toFixed() } degrees</p>
+      </div>
     );
   }
 }
